011b8ce97d59e52ebe780de6b1ea7c35d460a46c
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-04-26 16:24:23 +0500

    Adding index page and controller. closes #8.

---
+++ pet-clinic-web/src/main/java/ahmad/sf/sfpetclinic/controller/IndexController.java
@@ -0,0 +1,17 @@
+package ahmad.sf.sfpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value ={"","/", "index", "index.html"} , method = RequestMethod.GET)
+    public String index(Model model)
+    {
+        model.addAttribute("title", "Pet Clinic Index");
+        return "index";
+    }
+}
