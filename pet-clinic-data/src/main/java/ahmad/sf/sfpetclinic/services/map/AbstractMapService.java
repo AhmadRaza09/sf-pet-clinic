49d0a42562b527a359848f4fb70ea9f4af45f20d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-04-27 15:30:34 +0500

    refactored Map impl to abstract ID generation. closes #24

--- pet-clinic-data/src/main/java/ahmad/sf/sfpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/ahmad/sf/sfpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,17 @@
 package ahmad.sf.sfpetclinic.services.map;
 
+import ahmad.sf.sfpetclinic.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findALL()
     {
@@ -19,9 +23,21 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object)
+    T save(T object)
     {
-        map.put(id, object);
+        if(object != null)
+        {
+            if(object.getId() == null)
+            {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+
+        }
+        else
+        {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -33,5 +49,19 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object)
     {
         map.entrySet().removeIf(entry->entry.getValue().equals(object));
+    }
+
+    private Long getNextId()
+    {
+        Long nextId = null;
+        try
+        {
+            nextId = Collections.max(map.keySet()) + 1;
+        }
+        catch (NoSuchElementException e)
+        {
+            nextId = 1L;
+        }
+       return nextId;
     }
 }
