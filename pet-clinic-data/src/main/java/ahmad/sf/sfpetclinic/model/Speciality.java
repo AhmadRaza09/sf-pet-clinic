8f01a5266cda2ab5ae405fb57819e9786cdbaa4f
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-05-11 12:10:14 +0500

    Updated vet with JPA entity mappings. closes #37.

--- pet-clinic-data/src/main/java/ahmad/sf/sfpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ahmad/sf/sfpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package ahmad.sf.sfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
